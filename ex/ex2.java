fcb7bc7eb4215cf3e33ba5a315ea1a6711851a81
Function <42390852+algofunction@users.noreply.github.com>
2025-01-25 23:56:30 +0100

    Create ex2.java

---
+++ ex/ex2.java
@@ -0,0 +1,7 @@
+// fonction ordered envoie un booléen qui indique si 3 valeurs sont triées par ordre croissant
+
+static boolean ordered (int a, int b, int c){
+  if (a >= b >= c); 
+    return true;
+  return false;
+}
