6b0d9bb39ac2556b3bfba91cbb306e132ff6eaaf
Function <42390852+algofunction@users.noreply.github.com>
2025-01-30 00:07:42 +0100

    Update ex5.java

--- ex/ex5.java
+++ ex/ex5.java
@@ -1,3 +1,7 @@
+// fonction maxtab à un tableau d'entiers non vide, renvoyant en resultat la plus grande valeur du tableau
 static int maxtab(int[] tab) {
-  
+  int resultat = tab[0];
+  for (int i=1; i<tab.length; i++)
+    if(tab[i]>resultat) resultat = tab[i];
+  return resultat;
 }
