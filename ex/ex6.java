b68408958cfd890b82b574a3463e54eae81068af
Function <42390852+algofunction@users.noreply.github.com>
2025-01-31 00:01:41 +0100

    Update ex6.java

--- ex/ex6.java
+++ ex/ex6.java
@@ -2,6 +2,6 @@ static void inverse(int[] tab){
   for (int i = 0; i < tab.length / 2; i++){
     int swap = tab[i];
     tab[i] = tab[tab.length - 1 - i];
-    tab[tab.lengthi]
+    tab[tab.length - 1 - i] = swap;
   }
 }
