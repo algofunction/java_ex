05f035684eae02d1f946b27f5174ccc606a46732
Function <42390852+algofunction@users.noreply.github.com>
2025-02-07 23:21:41 +0100

    Update ex7.java

--- ex/ex7.java
+++ ex/ex7.java
@@ -1,7 +1,15 @@
 // définir une fonction qui s'applique à un tableau et renvoyant en résultat une copie du tableau, en utilisant une boucle de parcours.
+
 static int[] cptab(int[] tab) {
-	int[] resultat = new int[tab.length];
-	for(int i=0; i<tab.length; i++)
-		resultat[i] = tab[i];
-	return resultat;
+    // Déclaration et initialisation d'un tableau `resultat` de la même longueur que le tableau `tab`.
+    int[] resultat = new int[tab.length];
+    
+    // Boucle pour parcourir chaque élément du tableau d'entrée `tab`.
+    for (int i = 0; i < tab.length; i++) {
+        // Copier l'élément correspondant de `tab` dans le tableau `resultat`.
+        resultat[i] = tab[i];
+    }
+    
+    // Retourner le tableau `resultat`, qui est une copie exacte de `tab`.
+    return resultat;
 }
