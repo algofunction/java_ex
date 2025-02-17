fed92432f69b27d7a2f1db53e32bbc69353c0a9c
Function <42390852+algofunction@users.noreply.github.com>
2025-02-18 00:18:27 +0100

    Update Conversio.java

--- ex/Conversio.java
+++ ex/Conversio.java
@@ -1,30 +1,23 @@
 // Docu Scanner : https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html
-import java.util.Scanner; // Importation de la classe Scanner
+import java.util.Scanner; // Library Import 
 
+// Define Conversio class
 public class Conversio {
+    // Define main method
     public static void main(String[] args) {
         double euros;
         double dollars;
-        
-        // Création du type de classe Scanner et de la variable, création d'une instance de la classe Scanner via le constructeur Scanner(System.in) initialise l'objet  en lui passant en source, l'entrée standard
+        // Creation of the Scanner class type and the variable, creating an instance of the Scanner class using the Scanner(System.in) constructor initializes the object by providing the standard input as its source.
         Scanner scanner = new Scanner(System.in);
         // Display the message "Somme en euros?"  
         System.out.println("Somme en euros? ");
-        euros = scanner.nextDouble(); // Lire un nombre décimal saisi par l'utilisateur
+        euros = scanner.nextDouble(); // Read a decimal number entered by the user.
         
-        dollars = euros * 1.118; // Conversion en dollars
+        dollars = euros * 1.118; // Convert into dollar
         
         System.out.println("La somme en dollars: ");
         System.out.println(dollars);
         
-        scanner.close(); // Fermeture du Scanner pour libérer les ressources
+        scanner.close(); // Closing the Scanner to release resources
     }
 }
-
-/* Define Conversio class {
-    Define main method 
-        Define a float euros;
-        Define a float dollars;
-        
-
-**/
