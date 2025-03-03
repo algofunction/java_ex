2f111cdf592c42f40cf83853b7db25c51421a3ee
Function <42390852+algofunction@users.noreply.github.com>
2025-03-03 21:53:24 +0100

    Update ex1.java

--- ex/ex1.java
+++ ex/ex1.java
@@ -1,7 +1,7 @@
 // renvoie l'entier maximum
 
-static int max(int a, int b, int c) {
+static int max(int a, int b, int c){
   if(a>b && a>c) return a;
   if(b>c) return b;
-  return c;
+  else return c;
 }
