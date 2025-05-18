53793ceb90bbf078145757099b41c55dde28b737
Function <42390852+algofunction@users.noreply.github.com>
2025-05-18 22:52:20 +0200

    Create devoir_5.java

---
+++ ex/devoir_5.java
@@ -0,0 +1,75 @@
+
+public class devoir_5_ {
+	public static void main(String[]args) {
+		
+		
+		char[] voyelles = new char[] {a,e,i,o,u,y};
+		char[] consones = new char[] {B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z};
+		
+		public static string majusculesNonAccentuees(string, boolean);
+		
+		
+		
+		
+		public static string proprieteP(string, boolean);
+		i.length >= 3 char
+		tab[0]== consones 
+		tab[1][2]== voyelles 
+		
+		
+		
+		
+		int[] tab = new int[] {2,2,3};
+		System.out.println(tab[0]);
+		
+		
+		for(int i=0; i<tab.length ;i++)
+			System.out.println(tab[i]);//18.50
+		
+		
+		// tab 2 d
+		//int[][] tabdd = new int[2][3];
+		int[][] tabdd = 
+			{
+					{1,2,3},
+					{4,5,6}
+			};
+		
+		System.out.println(tabdd[1][2]);//1ere ligne 2e colonne
+		
+		
+		// 26.52 - 28.25 : une boucle par dimension pour tab 2 d
+		for(int i =0; i < tabdd.length ; i++)
+			for(int j = 0; j < tabdd[i].length ; j++)
+				// j < tabdd[i].length  car le parcour se fait par lignes
+				System.out.println(tabdd[i][j]);
+			
+		
+		
+		//int[][]
+				
+				// pour devoir 5
+				//tableau de voyelles puis on parcoure
+				// 6 mots compter le nbr de mots
+				// for on parcoure la chaine, accès à la chaine
+				// on a l'indice des blancs ac les boucles, on peut stocker les blancs
+				// avec substring on peut isoler chaque mot de la phrase
+				// on peut extraire chaque mot de la phrase
+				{}
+		
+		
+		
+		// déclarer deux tableaux int[] tab, tab2;
+		// déclarer un tableau et un int :: int tab[], tab2;
+		// jusqua 9.45 jc
+		// int[] tab = new int[3]; un tableau de 3 cases
+		//int[] tab = new int[] {1,2,3};//stockés de maniere contigue 
+		
+		//13.19 : capture d'ecran
+				
+				
+		//21.30 : tab 2 d 
+				
+					
+	}
+}
